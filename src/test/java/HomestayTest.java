2892b0071d11eebc55023863c5a8366eea59920b
Jackrit Yangsung <zcrltv@gmail.com>
2018-02-20 21:32:16 +0700

    [ADD] create testCheckHomestayNameRepeat()

--- src/test/java/HomestayTest.java
+++ src/test/java/HomestayTest.java
@@ -20,22 +20,24 @@ import static org.junit.Assert.*;
  * @author KazeShin
  */
 public class HomestayTest extends ConnectDatabaseTest {
-  private String pathToImage;
+
+    private String pathToImage;
+
     public HomestayTest() {
         initConnection();
         pathToImage = "/src/main/webapp/images/";
     }
-            
+
     @Test
-    public void testCreateHomestay(){
+    public void testCreateHomestay() {
         Map<String, Object> data = new HashMap<String, Object>();
 
         data.put("User_id", 1);
         data.put("Homestay_name", "huaghome");
-        data.put("Content_image", pathToImage+"test-image.jpg");
-        data.put("Cover_img", pathToImage+"test-cover-image.jpeg");
-        data.put("Hourse_document", pathToImage+"test-house-document.jpg");
-        data.put("Homestay_License_document", pathToImage+"test-house-document.jpg");
+        data.put("Content_image", pathToImage + "test-image.jpg");
+        data.put("Cover_img", pathToImage + "test-cover-image.jpeg");
+        data.put("Hourse_document", pathToImage + "test-house-document.jpg");
+        data.put("Homestay_License_document", pathToImage + "test-house-document.jpg");
         data.put("Description", "desc");
         data.put("Rule", "rule");
         data.put("Telno", "0123456789");
@@ -44,8 +46,13 @@ public class HomestayTest extends ConnectDatabaseTest {
         data.put("Close_time", "20.00");
         data.put("Status", "verifying");
         int expect = Homestay.createHomestay(data);
-        
+
         Assert.assertEquals(1, expect);
-   }
-    
+    }
+
+    @Test
+    public void testCheckHomestayNameRepeat() {
+        
+    }
+
 }
